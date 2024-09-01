153e694b7858750db05a81c5518c0b107b6e629f
Guilherme Lima Gonçalves <guligon90@gmail.com>
2024-09-01 01:56:13 -0300

    feat: add default content type to web config

--- src/main/java/com/infusetech/rest/orders/config/WebConfig.java
+++ src/main/java/com/infusetech/rest/orders/config/WebConfig.java
@@ -1,6 +1,8 @@
 package com.infusetech.rest.orders.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -14,5 +16,10 @@ public class WebConfig implements WebMvcConfigurer {
             .allowedOrigins("*")
             .allowedHeaders("*")
             .allowedMethods("GET", "PUT", "POST", "PATCH", "DELETE", "OPTIONS", "HEAD");
+    }
+
+    @Override
+    public void configureContentNegotiation(final ContentNegotiationConfigurer configurer) {
+        configurer.defaultContentType(MediaType.APPLICATION_JSON);
     }
 }
